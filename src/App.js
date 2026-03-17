22f54a771314080f4828eda204d626ed651c19b9
renxss <singhrenee80@gmail.com>
2026-03-17 20:33:48 +1100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,5 +2,17 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+      </div>
+      <footer>
+        This project was coded by Renee Singh and is {""}
+        <a href="https://github.com/renxss/react-weather-app2" target="_blank">
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
